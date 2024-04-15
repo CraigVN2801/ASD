3df4bd6068ae4335097d6ac2799440ccf5a23257
@Craigvn2801 <craigvn2801@gmail.com>
2024-04-15 19:38:23 +0200

    adding files

---
+++ adServer/var/cache/deliverycache_eecbef273bf7d3c68eb6a22207d3702f.php
@@ -0,0 +1,89 @@
+<?php
+
+$cache_contents   = array (
+  'cache_contents' => 
+  array (
+    'ad_id' => '1',
+    'placement_id' => '1',
+    'status' => '0',
+    'name' => 'Banner1',
+    'type' => 'html',
+    'contenttype' => '',
+    'pluginversion' => '0',
+    'filename' => '',
+    'imageurl' => '',
+    'htmltemplate' => '<html>
+<head>
+  <title>Title of the document</title>
+</head>
+
+<body>
+  <h1>This is a heading</h1>
+  <p>This is a paragraph.</p>
+  
+<div>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-NSEUHWmQsGxt4SfVM3f8VMW7vN8JsHnL-CnVII5E4A&s">
+</div>
+</body>
+
+</html>',
+    'htmlcache' => '<html>
+<head>
+  <title>Title of the document</title>
+</head>
+
+<body>
+  <h1>This is a heading</h1>
+  <p>This is a paragraph.</p>
+  
+<div>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-NSEUHWmQsGxt4SfVM3f8VMW7vN8JsHnL-CnVII5E4A&s">
+</div>
+</body>
+
+</html>',
+    'width' => '100',
+    'height' => '50',
+    'weight' => '1',
+    'seq' => '0',
+    'target' => '',
+    'url' => '',
+    'alt' => '',
+    'statustext' => '',
+    'bannertext' => '',
+    'adserver' => 'none',
+    'block_ad' => '0',
+    'cap_ad' => '0',
+    'session_cap_ad' => '0',
+    'compiledlimitation' => '',
+    'acl_plugins' => NULL,
+    'prepend' => '',
+    'append' => '',
+    'bannertype' => '0',
+    'alt_filename' => '',
+    'alt_imageurl' => '',
+    'alt_contenttype' => '',
+    'parameters' => 'N;',
+    'transparent' => '0',
+    'ext_bannertype' => 'bannerTypeHtml:oxHtml:genericHtml',
+    'iframe_friendly' => '1',
+    'campaign_id' => '1',
+    'block_campaign' => '86400',
+    'cap_campaign' => '100',
+    'session_cap_campaign' => '100',
+    'show_capped_no_cookie' => '0',
+    'client_id' => '1',
+    'clickwindow' => '0',
+    'viewwindow' => '0',
+    'advertiser_limitation' => '0',
+    'agency_id' => '1',
+    'campaign_status' => '0',
+  ),
+  'cache_name' => 'ad^1@localhost',
+  'cache_time' => 1713201558,
+  'cache_expire' => NULL,
+);
+
+$cache_complete   = true;
+
+?>
---
+++ adServer/var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2024-04-04 10:44:28
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
